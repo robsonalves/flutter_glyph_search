3e5c40b7bdfe36a34cf5d04c1090cdd1570ccbab
Robson Alves <robson.infoo@gmail.com>
2019-10-12 11:46:31 -0300

    new app

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:glyph_search/UI/HomePage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white
+    ),
+  ));
+}
+
