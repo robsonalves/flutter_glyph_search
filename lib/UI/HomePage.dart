09fbe11aa3a7da4469dbcef7303e19f5fc216431
Robson Alves <robson.infoo@gmail.com>
2019-10-13 00:12:22 -0300

    finished

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:glyph_search/UI/gif_page.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,7 +18,7 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGIFSearch() async {
     http.Response response;
 
-    if (_search == null) {
+    if (_search == null || _search.isEmpty) {
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=VcqNVbSGY1I7tyXZnjNeESoSDleozKFy&limit=20&rating=G");
     }
@@ -117,11 +119,13 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == null || index < snapshot.data["data"].length) {
             return GestureDetector(
-              child: Image.network(
-                  snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                   height: 300.0,
                   fit: BoxFit.cover
               ),
+
               onTap: () {
                 Navigator.push(context,
                     MaterialPageRoute(
@@ -130,6 +134,10 @@ class _HomePageState extends State<HomePage> {
                     )
                 );
               },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              },
+
             );
           } else {
             return Container(
